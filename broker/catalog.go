01aed63581c9c770442f138bd59d7090abb0ee74
Gerard Laan <prolane.org@gmail.com>
2018-11-09 14:36:22 +0100

    Improved catalog description

--- broker/catalog.go
+++ broker/catalog.go
@@ -14,14 +14,14 @@ func Catalog(w http.ResponseWriter, r *http.Request) {
 			{
 				Name:        "sso-oauth",
 				ID:          "dc081967-b7df-4bd4-8065-38415bb1e992",
-				Description: "Will require users to sign in to access your app. User can simply sign in with single sign on.",
+				Description: "Binding this service to your app will require users to sign in before they are allowed to access your app. Users can simply sign in using one of the offered single sign on solutions.",
 				Requires:    []string{"route_forwarding"},
 				Bindable:    true,
 				Plans: []*cf.Plan{
 					{
 						ID:          "abff1ea2-ad5e-44d4-9f00-036fcd5d16eb",
 						Name:        "google",
-						Description: "SSO is implemented using Google as identity provider. Users need to sign in with a valid corporate email address.",
+						Description: "SSO is implemented using Google as identity provider. Users need to sign in with a valid email address.",
 						Free:        true,
 					},
 				},
@@ -30,8 +30,7 @@ func Catalog(w http.ResponseWriter, r *http.Request) {
 	}
 	json, err := json.Marshal(catalog)
 	if err != nil {
-		fmt.Println("Um, how did we fail to marshal this catalog:")
-		//fmt.Printf("%# v\n", pretty.Formatter(catalog))
+		fmt.Println("Failed to marshal the catalog")
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte{})
 		return
